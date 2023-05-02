bb7b2cbec957ca9643ea3e377e21b60fa1c8d9ee
ftmzhrsckldr <ftmzhrsckldr>
2023-05-02 10:16:59 +0300

    Debug mode and variable names changed

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # Appcircle _Data Theorem_ component
 
-Upload to Data Theorem
+Scan your app using Data Theorem Mobile Secure.
 
 ## Required Inputs
 
-- `AC_DT_FILE_PATH`: File path to the IPA or APK to upload.  This variable can for example be set to:
-   - `$AC\_APK\_PATH` after the `Android Build` step
-   - `$AC\_APK\_PATH` or `$AC\_SIGNED\_APK\_PATH` after the "Android Sign" step
-    - `$AC\_IPA\_PATH` after the "Xcode Archive & Export for iOS" step
+- `AC_DT_FILE_PATH`: File path to the IPA or APK to upload. This variable can for example be set to:
+   - `$AC_APK_PATH` after the "Android Build" step
+   - `$AC_APK_PATH` or `$AC_SIGNED_APK_PATH` after the "Android Sign" step
+   
+   - `$AC_EXPORT_DIR/Myapp.ipa` after the "Xcode Archive & Export for iOS" step
 
-- `AC_DT_UPLOAD_API_KEY`: Data Theorem Upload API Key. You will need to retrieve your organization's Upload API key from the portal.
+- `AC_DT_UPLOAD_API_KEY`: Data Theorem Mobile Secure upload API key. You will need to retrieve your organization's Upload API key from the portal.
--- main.sh
+++ main.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-set -ex
 shopt -s nocasematch
 
 # Check required environment variables
 if [ -z "${AC_DT_UPLOAD_API_KEY}" ]
 then
-    printf 'Missing input: AC_DT_UPLOAD_API_KEY\n' >&2
+    echo 'Missing input: AC_DT_UPLOAD_API_KEY\n' >&2
     exit 1
 elif [[ ${AC_DT_UPLOAD_API_KEY} =~ APIKey* ]]
 then
-    printf 'Variable AC_DT_UPLOAD_API_KEY should not start with "APIKey"\n' >&2
+    echo 'Variable AC_DT_UPLOAD_API_KEY should not start with "APIKey"\n' >&2
     exit 1
 fi
 
 if [ -z "${AC_DT_FILE_PATH}" ]
 then
-    printf 'Missing input: AC_DT_FILE_PATH\n' >&2
+    echo 'Missing input: AC_DT_FILE_PATH\n' >&2
     exit 1
 elif [ ! -f "${AC_DT_FILE_PATH}" ]
 then
-    printf 'App binary file not found\n' >&2
+    echo 'App binary file not found\n' >&2
     exit 1
 fi
 
@@ -31,7 +30,7 @@ max_retries=3
 for (( retry = 0; retry < max_retries; retry++ ))
 do
     # Step 1: get the upload URL
-    printf 'Generating an unique upload URL\n'
+    echo 'Generating an unique upload URL\n'
 
     # Create a session:
     #   - The output on stdout is the HTTP status (200 on success);
@@ -50,19 +49,19 @@ do
     # If the previous request failed, show an error on stdout, and retry.
     if [ "${http_status}" -ne 200 ]
     then
-        printf '[%s] Failure while getting the upload URL\n' "${retry}" >&2
-        printf '[%s] HTTP status was: %s\n' "${retry}" "${http_status}" >&2
-        printf '[%s] HTTP response was:\n' "${retry}" >&2
+        echo '[%s] Failure while getting the upload URL\n' "${retry}" >&2
+        echo '[%s] HTTP status was: %s\n' "${retry}" "${http_status}" >&2
+        echo '[%s] HTTP response was:\n' "${retry}" >&2
         cat "${tempfile}" >&2
         continue
     fi
 
     # Get the upload URL from the response
     upload_url=$(jq -r '.upload_url' < "${tempfile}")
-    printf '%s\n' "${upload_url}"
+    echo '%s\n' "${upload_url}"
 
     # Step 2: upload the app build
-    printf 'Uploading app build to Data Theorem\n'
+    echo 'Uploading app build to Data Theorem\n'
 
     # Perform the upload:
     #   - The output on stdout is the HTTP status (200 on success);
@@ -79,9 +78,9 @@ do
     # If the previous request failed, show an error on stdout, and retry.
     if [ "${http_status}" -ne 200 ]
     then
-        printf '[%s] Failure while uploading the build\n' "${retry}" >&2
-        printf '[%s] HTTP status was: %s\n' "${retry}" "${http_status}" >&2
-        printf '[%s] HTTP response was:\n' "${retry}" >&2
+        echo '[%s] Failure while uploading the build\n' "${retry}" >&2
+        echo '[%s] HTTP status was: %s\n' "${retry}" "${http_status}" >&2
+        echo '[%s] HTTP response was:\n' "${retry}" >&2
         cat "${tempfile}" >&2
         continue
     fi
@@ -92,6 +91,6 @@ done
 
 if [ "${retry}" -ge "${max_retries}" ]
 then
-    printf 'Upload failed after %s attempts\n' "${max_retries}"
+    echo 'Upload failed after %s attempts\n' "${max_retries}"
     exit 1
 fi
